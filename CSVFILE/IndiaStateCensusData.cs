e7df8874d5b5cfd41ca074e4093ab24dc27be264
kn52 <ashish52922@gmail.com>
2020-08-08 14:12:15 +0530

    Add project files.

---
+++ CSVFILE/IndiaStateCensusData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVFILE
+{
+    class IndiaStateCensusData
+    {
+        public string State { get; set; }
+        public string Population { get; set; }
+        public double AreaInSqKm { get; set; }
+        public double DensityPerSqKm { get; set; }
+    }
+}
---
+++ CSVFILE/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using CsvHelper;
+
+namespace CSVFILE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\Users\\Kajal\\Downloads\\Compressed\\Project\\IndiaStateCensusData.csv";
+            //string[] data = File.ReadAllLines(path);
+            //foreach(string s in data)
+            //{
+            //    Console.WriteLine(s);
+            //}
+            List<IndiaStateCensusData> census = new List<IndiaStateCensusData>(); 
+            using (TextReader reader = new StreamReader(path))
+            {
+                CsvReader csv = new CsvReader(reader);
+                csv.Configuration.Delimiter = ";";
+                csv.Configuration.MissingFieldFound = null;
+                while (csv.Read())
+                {
+                    IndiaStateCensusData Record = csv.GetRecord<IndiaStateCensusData>();
+                    census.Add(Record);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
